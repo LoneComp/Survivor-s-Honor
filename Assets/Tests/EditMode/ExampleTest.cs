de4b4f2cfd3b26a553dc0be4dc4f06b65fa76f33
LoganSanz <114677579+M0B3@users.noreply.github.com>
2026-07-03 15:42:03 +0200

    FT_ add example EditMode test

---
+++ Assets/Tests/EditMode/ExampleTest.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace Tests.EditMode
+{
+    /// <summary>
+    /// Test d'exemple pour amorcer la suite de tests EditMode.
+    /// Remplace-le par de vrais tests de ta logique de jeu.
+    /// </summary>
+    public class ExampleTest
+    {
+        [Test]
+        public void Example_TwoPlusTwo_IsFour()
+        {
+            Assert.AreEqual(4, 2 + 2);
+        }
+    }
+}
